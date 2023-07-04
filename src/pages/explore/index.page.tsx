305bff54ba1d50c2d460ed0a5e0a72aab4cd922c
Rhalf Oliveira <rhalfsouzaa@gmail.com>
2023-07-04 16:38:15 -0300

    feat: create explore page and search

--- src/pages/explore/index.page.tsx
+++ src/pages/explore/index.page.tsx
@@ -1,3 +1,30 @@
-export default function Explore() {
-  return <h1>Explore</h1>
+import { Binoculars, MagnifyingGlass } from 'phosphor-react'
+import { PageTitle } from '../../components/PageTitle'
+import { DefaultLayout } from '../../layouts/DefaultLayout'
+import { NextPageWithLayout } from '../_app.page'
+import { Container, Search, SearchContent } from './styles'
+
+const Explore: NextPageWithLayout = () => {
+  return (
+    <Container>
+      <PageTitle
+        title="Explorar"
+        icon={<Binoculars size={32} />}
+        css={{ marginBottom: '2.75rem' }}
+      />
+
+      <Search>
+        <SearchContent>
+          <input placeholder="Buscar livro ou autor" />
+          <MagnifyingGlass size={20} />
+        </SearchContent>
+      </Search>
+    </Container>
+  )
 }
+
+Explore.getLayout = (page) => {
+  return <DefaultLayout title="Explorar">{page}</DefaultLayout>
+}
+
+export default Explore
