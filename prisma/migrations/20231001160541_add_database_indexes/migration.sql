d4eb3770bcb4ee470eaa51ed43558b18792e935a
Rhalf Oliveira <rhalfsouzaa@gmail.com>
2023-10-01 16:59:23 -0300

    feat: update prisma relatations

---
+++ prisma/migrations/20231001160541_add_database_indexes/migration.sql
@@ -0,0 +1,29 @@
+-- DropForeignKey
+ALTER TABLE `CategoriesOnBooks` DROP FOREIGN KEY `CategoriesOnBooks_book_id_fkey`;
+
+-- DropForeignKey
+ALTER TABLE `CategoriesOnBooks` DROP FOREIGN KEY `CategoriesOnBooks_categoryId_fkey`;
+
+-- DropForeignKey
+ALTER TABLE `accounts` DROP FOREIGN KEY `accounts_user_id_fkey`;
+
+-- DropForeignKey
+ALTER TABLE `ratings` DROP FOREIGN KEY `ratings_book_id_fkey`;
+
+-- DropForeignKey
+ALTER TABLE `ratings` DROP FOREIGN KEY `ratings_user_id_fkey`;
+
+-- DropForeignKey
+ALTER TABLE `sessions` DROP FOREIGN KEY `sessions_user_id_fkey`;
+
+-- CreateIndex
+CREATE INDEX `CategoriesOnBooks_book_id_idx` ON `CategoriesOnBooks`(`book_id`);
+
+-- RenameIndex
+ALTER TABLE `CategoriesOnBooks` RENAME INDEX `CategoriesOnBooks_categoryId_fkey` TO `CategoriesOnBooks_categoryId_idx`;
+
+-- RenameIndex
+ALTER TABLE `ratings` RENAME INDEX `ratings_book_id_fkey` TO `ratings_book_id_idx`;
+
+-- RenameIndex
+ALTER TABLE `ratings` RENAME INDEX `ratings_user_id_fkey` TO `ratings_user_id_idx`;
